d805119c490271b52d988628cff9af511ed833a4
Brycen <dorsay@live.ca>
2017-03-14 15:31:30 -0700

    log original message

--- subscribe.js
+++ subscribe.js
@@ -8,6 +8,7 @@ rabbit.connect(config.rabbit_url).then(connection => {
   return connection.createChannel().then(channel => {
     channel.consume(queue, message => {
       if(!message.consumerTag) channel.ack(message);
+      console.log(message);
       message = JSON.parse(message.content.toString());
       telegram.process(connection, message);
     });
